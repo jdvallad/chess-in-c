b897bde7802e583f21fbb8351a4acd611a4e76ee
Joshua Valladares <joshuadvalladares@gmail.com>
2023-03-20 19:07:22 -0700

    init commit

---
+++ Chess.c
@@ -0,0 +1,13 @@
+#include <stdio.h> // define the header file
+#include <stdbool.h> // booleans!
+#include <stdint.h> // ints!
+#define HASH_LENGTH 70
+int main()   // define the main function
+{
+    printf("Welcome to JavaTpoint\r\n");  // print the statement.
+    return 0;
+}
+
+struct Chess{
+  char* hash;
+};
